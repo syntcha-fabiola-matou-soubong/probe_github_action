14de97c26f681c2b2b366ee088a3b4c85f55246c
sm90073 <syntcha.fabiola.matou.soubong@feynsinn.de>
2023-07-24 08:20:36 +0200

    update style and tod.spec.js file

--- frontend/todo-spec.js
+++ frontend/todo-spec.js
@@ -1,22 +1,22 @@
 describe('angularjs homepage todo list', function() {
   it('should display title and button', function() {
-    browser.waitForAngularEnabled(false); // Désactiver l'attente d'Angular
+    browser.waitForAngularEnabled(false);
 
     browser.get('http://localhost:4200/');
 
-   
-  var nameInput = element(by.id('name'));
-  var emailInput = element(by.id('email'));
-  var submitButton = element(by.css('button[type="submit"]'));
+    var nameInput = element(by.id('name'));
+    var emailInput = element(by.id('email'));
+    var genderInput = element(by.id('gender'));
+    var submitButton = element(by.css('button[type="submit"]'));
 
-  
-  nameInput.sendKeys('John Doe');
-  emailInput.sendKeys('john@example.com');
+    nameInput.sendKeys('John Doe');
+    emailInput.sendKeys('john@example.com');
+    genderInput.sendKeys('male');
 
+    submitButton.click();
 
-  submitButton.click();
-
-  expect(nameInput.getAttribute('value')).toEqual('');
-  expect(emailInput.getAttribute('value')).toEqual('');
-});
+    expect(nameInput.getAttribute('value')).toEqual('');
+    expect(emailInput.getAttribute('value')).toEqual('');
+    expect(genderInput.getAttribute('value')).toEqual('');
+  });
 });
